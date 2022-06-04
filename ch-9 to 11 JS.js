a498999be20afb72097bd615f4937f44234ad668
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-06-04 15:42:56 +0500

    start Q no 2

--- ch-9 to 11 JS.js
+++ ch-9 to 11 JS.js
@@ -2,7 +2,7 @@
 // user enters “Karachi”, welcome the user like this:
 //  “Welcome to city of lights” -->
 
-function submit() {
+function checkCity() {
 
     let input1 = document.querySelector('#city').value;
     console.log("input1", input1);
@@ -19,8 +19,36 @@ function submit() {
 
     console.log("result", result);
 
-    let sumResultElement = document.querySelector('#sumResult');
+    let ResultElement = document.querySelector('#Result');
 
-    sumResultElement.innerText = result;
+    ResultElement.innerText = result;
 
 }
+
+// <!-- 2. Write a program to take “gender” as input from user. If the
+//     user is male, give the message: Good Morning Sir. If the
+//     user is female, give the message: Good Morning Ma’am. -->
+ 
+function checkCity() {
+
+    let input1 = document.querySelector('#city').value;
+    console.log("input1", input1);
+
+    // let cityName = "karachi";
+
+    let result = "";
+
+    if (input1 == "karachi" || input1 == "Karachi" || input1 == "Khi" || input1 == "khi" ) {
+        result = "Welcome to city of lights";
+    }else {
+        result = "One Of the city of Pakistan"
+    }
+
+    console.log("result", result);
+
+    let ResultElement = document.querySelector('#Result');
+
+    ResultElement.innerText = result;
+
+}
+
