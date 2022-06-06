768faeb1ea08f50aedbaa3948c570b4962ef9ec3
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-06-06 16:21:12 +0500

    start Q no 7 of Chapter 9 to 11

--- ch-9 to 11 JS.js
+++ ch-9 to 11 JS.js
@@ -291,3 +291,11 @@ function markSheet() {
 
 
     }
+
+    // 7. Guess game:
+    // Store a secret number (ranging from 1 to 10) in a variable.
+    // Prompt user to guess the secret number.
+    // a. If user guesses the same number, show “Bingo! Correct
+    // answer”.
+    // b. If the guessed number +1 is the secret number, show
+    // “Close enough to the correct answer”.
