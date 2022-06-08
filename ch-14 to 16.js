e52642f1d617d6e2971fc6cc4edbc0f5dd069421
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-06-08 20:34:43 +0500

    Q no 1 to 6 of chapter 14 to 16

---
+++ ch-14 to 16.js
@@ -0,0 +1,9 @@
+let studentName = [] ; 
+let emptyArr = [] ;
+let stringArr = [ "a" , "b" , "c" , "d" , "e" , "f"] ;
+let numberArr = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ] ;
+
+let mixArr = [ 1 , 2 , "a" , "b" , 3 , "f"] ;
+
+let dgree = [ " SSC " , " HSC " , " BSC " , " BS " , " BCOM " , " MS " , " M. Phil " , " PhD " ] ;
+
